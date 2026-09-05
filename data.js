efbf76a4df68f0871b8560513b6c40bf4103b39c
rmart4 <r.marteletti@gmail.com>
2026-09-05 15:14:03 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,22 +1,23 @@
 // Banque de questions : image exacte extraite de la fiche "Vocabulaire technique"
 // + terme correct (issu de la fiche de synthèse STI2D 2I2D I.T.E.C.)
+// Chaque association a été revérifiée individuellement contre les définitions officielles.
 const QUESTION_BANK = [
   { img: "alesage_1.png", answer: "Alésage" },
   { img: "arbre_1.png", answer: "Arbre" },
   { img: "arrondi_1.png", answer: "Arrondi" },
+  { img: "arrondi_2.png", answer: "Arrondi" },
   { img: "bossage_1.png", answer: "Bossage" },
   { img: "goutte_de_suif_1.png", answer: "Goutte de suif" },
   { img: "goutte_de_suif_2.png", answer: "Goutte de suif" },
   { img: "chanfrein_1.png", answer: "Chanfrein" },
   { img: "chanfrein_2.png", answer: "Chanfrein" },
-  { img: "chanfrein_3.png", answer: "Chanfrein" },
-  { img: "conge_1.png", answer: "Congé" },
-  { img: "conge_2.png", answer: "Congé" },
   { img: "chambrage_1.png", answer: "Chambrage" },
   { img: "collet_1.png", answer: "Collet" },
+  { img: "collerette_1.png", answer: "Collerette" },
+  { img: "conge_1.png", answer: "Congé" },
   { img: "degagement_1.png", answer: "Dégagement" },
   { img: "dent_1.png", answer: "Dent" },
-  { img: "entaille_1.png", answer: "Entaille" },
+  { img: "decrochement_1.png", answer: "Décrochement" },
   { img: "epaulement_1.png", answer: "Epaulement" },
   { img: "ergot_1.png", answer: "Ergot" },
   { img: "evidement_1.png", answer: "Evidement" },
@@ -36,7 +37,6 @@ const QUESTION_BANK = [
   { img: "rainure_3.png", answer: "Rainure" },
   { img: "saignee_1.png", answer: "Saignée" },
   { img: "embase_1.png", answer: "Embase" },
-  { img: "semelle_1.png", answer: "Semelle" },
   { img: "teton_1.png", answer: "Téton" },
 ];
 
